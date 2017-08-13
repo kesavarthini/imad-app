be9c54de52004c5067f4875b11a4885e4f9c7d21
Kesavarthini Ilanchezhian <ickesavarthini@gmail.com>
2017-08-13 11:39:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ element.innerHTML ='New-value';
 var img = document.getElementById("madi");
 img.onclick=function()
 {
-    img.style.marginLeft ='100px';
+    var interval= setInterval(moveLeft,100);
 };
