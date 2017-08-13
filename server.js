50aebb82da747485177d86f2a2996d85664c3b18
Kesavarthini Ilanchezhian <ickesavarthini@gmail.com>
2017-08-13 10:05:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one.html', function (req, res) {
-  res.send('The article one requested will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two.html', function (req, res) {
