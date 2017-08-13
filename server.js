9154453d395a4bae4d2e502aa09609f220114ef4
Kesavarthini Ilanchezhian <ickesavarthini@gmail.com>
2017-08-13 10:28:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content={
+  title: 'Article one | Kesavarthini',
+  heading: 'Article one',
+  date: '13th august 2017',
+  content:` <p>
+            This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one.
+        </p>
+        <p>
+            This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one.
+        </p>
+         <p>
+            This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one. This is the content of article one.
+        </p>`
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
