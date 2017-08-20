1f6d8c54b40bfbea4f8356c2aee241a3e1d81614
Kesavarthini Ilanchezhian <ickesavarthini@gmail.com>
2017-08-20 12:22:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,11 +13,13 @@ img.onclick=function()
         
     }
 };
+
 */var counter=0;
 //counter code
 var button=document.getElementById("counter");
 button.onclick=function(){
     //make a req to the counter and capture the response and store in it a variable,render the variablr in the correct 
+    var request = new XMlhttpRequest();
     counter= counter + 1;
     var span=document.getElementById("count");
     span.innerHTML=counter.toString();
